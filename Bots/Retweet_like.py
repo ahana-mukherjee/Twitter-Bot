841b0c13e775f3c7847051a7d5f7186ff51d645c
arnab-2001 <arnabbibhuti01@gmail.com>
2021-09-19 20:03:18 +0530

    Added files

--- Bots/Retweet_like.py
+++ Bots/Retweet_like.py
@@ -18,7 +18,6 @@ class FavRetweetListener(tweepy.StreamListener):
         logger.info(f"Processing tweet id {tweet.id}")
         if tweet.in_reply_to_status_id is not None or \
             tweet.user.id == self.me.id:
-            # This tweet is a reply or I'm its author so, ignore it
             return
         if not tweet.favorited:
             # Mark it as Liked, since we have not done it yet
